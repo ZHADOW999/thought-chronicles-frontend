35f35b5bbed71135f2590be5af48851663d354cb
Akashii0 <abdulkid151@gmail.com>
2024-12-03 18:52:29 +0100

    Updated api calls to localhost

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tc.a.7o7.cx',  // Your FastAPI backend URL
+  // baseURL: 'https://tc.a.7o7.cx',  // Your FastAPI backend URL
+  baseURL: 'http://localhost:4000',  // Your FastAPI backend URL
   withCredentials: true,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,8 @@ import UseFetch from "../components/useFetch";
 
 
 const Home = () => {
-    const {data:blogs ,loading,error} = UseFetch("https://tc.a.7o7.cx/api/blogs")
+    // const {data:blogs ,loading,error} = UseFetch("https://tc.a.7o7.cx/api/blogs")
+    const {data:blogs ,loading,error} = UseFetch("http://localhost:4000/api/blogs")
     return ( 
         <div className="">
             {error &&<p className="text-5xl text-center uppercase">{error}</p>}
