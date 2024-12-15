9a464f9fa4f564fad5f33c9971ea78cee2709389
Akashii0 <abdulkid151@gmail.com>
2024-12-15 22:43:32 +0100

    Build delete

--- src/components/profilePic.js
+++ src/components/profilePic.js
@@ -18,7 +18,7 @@ const ProfilePicture = ({ userId, fallbackImage, altText = "Profile Picture" }) 
         setError(false);
         setProfilePic(null);
 
-        const response = await fetch(`http://127.0.0.1:4000/api/users/${userId}/profile-picture`);
+        const response = await fetch(`https://tc.a.7o7.cx/api/users/${userId}/profile-picture`);
         if (!response.ok) {
           throw new Error("Failed to fetch profile picture");
         }
@@ -35,15 +35,17 @@ const ProfilePicture = ({ userId, fallbackImage, altText = "Profile Picture" }) 
   }, [userId]); // Re-run the effect when userId changes
 
   if (error && fallbackImage) {
-    return <div class="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-    <svg class="absolute w-20 h-24 text-gray-400 -left-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+    return <div className="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <svg className="absolute w-20 h-24 text-gray-400 -left-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
 </div>
   }
 
   return profilePic ? (
     <img className="w-20 h-20  rounded-full" src={profilePic} alt={altText} />
   ) : (
-    <p>Loading...</p>
+    <div className="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <svg className="absolute w-20 h-24 text-gray-400 -left-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+</div>
   );
 };
 
