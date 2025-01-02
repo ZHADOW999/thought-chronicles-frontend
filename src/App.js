08634777fa1c3928edbd669bf7eafdeff983a1da
ZHADOW999 <babalolav9@gmail.com>
2025-01-02 14:17:51 +0100

    Update asset manifest and refactor App.js for improved performance and UI consistency
    - Updated asset manifest to reflect new CSS and JS file names after build changes.
    - Removed obsolete CSS and JS files to streamline the build process.
    - Refactored App.js to restore direct imports of components, enhancing clarity and maintainability.
    - Adjusted routing structure for better performance and user experience.
    
    These changes collectively enhance the application's performance, maintainability, and user interface consistency.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
-import React, { Suspense } from "react";
+import React from "react";
 import MainLayout from "./components/mainLayout";
-// import Home from "./pages/home"
+import Home from "./pages/home"
 
 import EditBlog from "./components/editBlog";
 import AuthForm from "./login/AuthForm";
 import NotFound from "./pages/404 page/404page";
-// import BlogDetails from "./components/blogDetails";
-// import Profile from "./pages/profiles";
-// import BlogCreateSection from "./components/BlogCreateSection";
-// import Stories from "./pages/stories"; 
-// import Library from "./pages/libraries";
+import BlogDetails from "./components/blogDetails";
+import Profile from "./pages/profiles";
+import BlogCreateSection from "./components/BlogCreateSection";
+import Stories from "./pages/stories"; 
+import Library from "./pages/libraries";
 
 
 function App() {
-const Home = React.lazy(()=>import("./pages/home"))
-const BlogDetails = React.lazy(()=> import("./components/blogDetails"))
-const Profile = React.lazy(()=> import("./pages/profiles"))
-const BlogCreateSection = React.lazy(()=> import("./components/BlogCreateSection"))
-const Stories = React.lazy(()=> import("./pages/stories"))
-const Library = React.lazy(()=> import("./pages/libraries"))
+// const Home = React.lazy(()=>import("./pages/home"))
+// const BlogDetails = React.lazy(()=> import("./components/blogDetails"))
+// const Profile = React.lazy(()=> import("./pages/profiles"))
+// const BlogCreateSection = React.lazy(()=> import("./components/BlogCreateSection"))
+// const Stories = React.lazy(()=> import("./pages/stories"))
+// const Library = React.lazy(()=> import("./pages/libraries"))
 
   return (
     <Router>
-      <Suspense fallback={<p ></p>}>
+      
       <Routes>
         <Route exact path="/login" element={<AuthForm/>}/>
           <Route  path="/" element={<MainLayout><Home/></MainLayout>}/>
@@ -35,7 +35,7 @@ const Library = React.lazy(()=> import("./pages/libraries"))
           <Route path="/library" element={<MainLayout><Library /></MainLayout>}/>
           <Route path="*" element={<NotFound/>}/>
       </Routes>
-      </Suspense>
+      
     </Router>
   );
 }
